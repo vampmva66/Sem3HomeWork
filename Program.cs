7f0a766b6db7d0c985b80da10740bbeb34e4a096
Vyacheslav Minaev <slavik.minaeff@yandex.ru>
2023-08-02 00:45:53 +0300

    Сделал вторую задачу

--- Program.cs
+++ Program.cs
@@ -25,3 +25,16 @@
 // double s = Math.Sqrt ((Math.Pow((X2-X1),2) + Math.Pow((Y2-Y1),2) + Math.Pow((Z2-Z1),2)));
 
 // Console.WriteLine(s);
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= N; i++ )
+// {
+//     Console.WriteLine(Math.Pow(i,3));
+// }
